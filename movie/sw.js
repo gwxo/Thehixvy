34e5d2fe131fc4f248de4a77653669c4131f0182
Gw team <codewithprashant07@gmail.com>
2026-04-09 18:53:51 +0530

    Update sw.js

--- movie/sw.js
+++ movie/sw.js
@@ -1,3 +1,4 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 const CACHE_NAME = 'prasxmovie-v2.1'; // Bumped version to force update
 
 // Update these paths to point specifically to your movie folder assets
