609517f70957908287f31a5535c1db45898b96ed
Gw team <codewithprashant07@gmail.com>
2026-04-11 08:34:46 +0530

    Create firebase-config.js

---
+++ Ai/firebase-config.js
@@ -0,0 +1,10 @@
+// Paste your Firebase configuration object here
+// This is an example, use your own!
+export const firebaseConfig = {
+  apiKey: "AIzaSyB...xxxxxxxxxxxxxxxx",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:xxxxxxxxxxxxxxxxx"
+};
